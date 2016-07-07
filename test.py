d1daf6d83763460f0095e3b927c4b0c423721841
Trelay Wang <trelwan@celestica.com>
2016-07-07 12:14:34 +0800

    Update for reset

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 from htmllog import *
+log_format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
 
-logger = HTMLLogger(filename='test_log.html', mode='a', title='Test_Title',version='1.2', level=10)
+logger = PyLogger(filename='test_log.html', html_format=log_format,
+		mode='a', title='Test_Title', level=10, console_log=True)
+xxx='ddddddd'
 
-for i in range(30):
+for i in range(10):
 	logger.debug('This is debug')
 	logger.info('This is info')
-	logger.warning('This is warning')
-	logger.error('This is error')
+	logger.warning("This is <hl>warning</hl>")
+	logger.error('This is <hl>error</hl>'+xxx)
 	logger.info('_____________')
