4b3f73093462a9a2381e7a38296c0efabccf016d
Trelay wang <trelwan@celestica.com>
2018-08-11 13:46:52 +0000

    add unittest

--- test.py
+++ test.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
-from PyLog2html import *
-logger=PyLogger(html_filename='test.html',console_log=True)
+import unittest
 
+class Test(unittest.TestCase):
+    def setUp(self):
+        from HTMLLogger import HTMLLogger
+        self.log_nm = "log.html"
+        self.logger = HTMLLogger(name="Test_App", html_filename=self.log_nm ,console_log=True)
+
+    def tearDown(self):
+        import os
+        os.remove(self.log_nm)
+
+    def test_logger(self):
+        logger = self.logger
+        for i in range(1):
+            logger.debug('This is debug')
+            logger.info('This is info')
+            logger.warning("This is <hl>warning</hl>")
+            logger.error('This is <hl>error</hl> xxx')
+            logger.info('_____________')
+            logger.table('Add html table:<table><tr><th>...</th></tr></table>')
 if __name__=="__main__":
-	for i in range(6):
-		logger.debug('This is debug')
-		logger.info('This is info')
-		logger.warning("This is <hl>warning</hl>")
-		logger.error('This is <hl>error</hl> xxx')
-		logger.info('_____________')
-        logger.table('Add html table:<table><tr><th>...</th></tr></table>')
+    unittest.main()
